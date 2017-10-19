a68471bda4c726136b0f84ad8397b110e6bcf483
Emma Freeman <freemoe04@gmail.com>
2017-10-19 16:25:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,3 @@
-ruby
-def display_board
- puts "A Tic Tac Toe Board"
-end
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -10,3 +6,4 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
